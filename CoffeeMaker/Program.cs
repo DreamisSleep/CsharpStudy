793612ba7ecc55907893f71894b8e93e23ab47fe
HyunSeoPark <goatsfish@naver.com>
2020-09-10 23:43:08 +0900

    C#
    syu study

---
+++ CoffeeMaker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMaker
+{
+    class CoffeeMakerApp
+    {
+        public bool onState;
+        public void StartCoffeemaker()
+        {
+            if (onState == true)
+                Console.WriteLine("The coffeMaker is already on");
+            else
+                onState = true;
+                Console.WriteLine("The coffeMaker is now on");
+        }
+        static void Main(string[] args)
+        {
+            CoffeeMakerApp st = new CoffeeMakerApp();
+            st.onState = true;
+            st.StartCoffeemaker();
+        }
+    }
+}
