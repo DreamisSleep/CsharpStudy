b8fd2c8418faea335504d1b1817764a75e3e1af0
HyunSeoPark <goatsfish@naver.com>
2020-09-22 23:19:00 +0900

    C#
    syu class..
    ing...

---
+++ 박현서2017100922실습/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace 박현서2017100922실습
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(500, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "배경색 변경";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(129, 470);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "배경색 변경 가능";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 470);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(245, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "배경색 변경 불가";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(129, 532);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(245, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "영역확대";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(384, 532);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(245, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "영역축소";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(275, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(763, 621);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "박현서2017100922실습3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 박현서2017100922실습/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 박현서2017100922실습
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowIcon = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Random r = new Random();
+            Color c = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+            g.FillRectangle(new SolidBrush(c), e.ClipRectangle);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Enabled = false;
+            button3.Enabled = true;
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Expand();
+        }
+
+
+
+        private void Expand() // 영역확대 메소드
+        {
+
+            if (panel1.Width < 300)
+            {
+
+                panel1.Size =
+                    new Size(panel1.Size.Width * 11 / 10, panel1.Size.Height * 11 / 10);
+                // 이전 배경 색상으로 변경
+            }
+            else
+            {
+                MessageBox.Show("최대 크기입니다.");
+            }
+            panel1.BackColor = panel1.ForeColor;
+            panel1.BackColor = SystemColors.Control;
+        }
+
+        private void Reduce() // 영역축소 메소드
+        {
+            if (panel1.Width < 50)
+            {
+                MessageBox.Show("최소 크기입니다.");
+            }
+            else
+            {
+                panel1.Size =
+                    new Size(panel1.Size.Width * 10 / 11, panel1.Size.Height * 10 / 11);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Reduce();
+        }
+
+
+        private void panel1_Resize(object sender, EventArgs e) // 가로 좌표 고정
+        {
+            panel1.Left = (this.ClientSize.Width - panel1.Width) / 2;
+            panel1.Top = (this.ClientSize.Height- button1.Width)/2;
+        }
+
+
+    }
+}
